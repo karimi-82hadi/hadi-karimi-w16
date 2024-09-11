996734a585e295389afbbd2ec55c23b705a24ec1
Hadi Karimi <karimi1382hadi@gmail.com>
2024-09-12 00:56:22 +0330

    developed CityInput component and defined searchHandler function to get cities.

--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+
+import cities from "../cities.json";
+
 import styles from "./CityInput.module.css";
 
-const CityInput = ({ handleChange, hint }) => {
+const CityInput = () => {
+  const allCities = cities;
+  const [searchedCities, setSearchedCities] = useState([]);
+  const [cityName, setCityName] = useState("");
+  const cityFullName = cityName.length ? searchedCities[0] : "";
+
+  const searchHandler = (e) => {
+    const filteredCities = allCities.filter((item) =>
+      item.startsWith(e.target.value)
+    );
+    setSearchedCities(filteredCities);
+  };
+
+  const keyPressHandler = (e) => {
+    if (e.key === "Enter") {
+      setCityName(cityFullName);
+    }
+  };
+
   return (
     <div className={styles.input}>
       <h1>Input Mask Mini Project</h1>
       <div className={styles.inputInner}>
-        <label htmlFor="input">{hint}Hadi Karimi</label>
-        <input type="text" id="input" onChange={handleChange} value="Hadi" />
+        <label htmlFor="input">{cityFullName}</label>
+        <input
+          type="text"
+          id="input"
+          placeholder="Enter City Name"
+          onChange={(e) => setCityName(e.target.value)}
+          onInput={searchHandler}
+          onKeyPress={keyPressHandler}
+          value={cityName}
+        />
       </div>
     </div>
   );
